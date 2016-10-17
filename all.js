aefae93d34ebe1505f23fff77dbba75a832792af
panda <elmas87532@gmail.com>
2016-10-17 18:58:19 +0800

    jsAndcss

---
+++ all.js
@@ -0,0 +1,10 @@
+var a = parseInt(document.getElementById("a").value);
+var b = parseInt(document.getElementById("b").value);
+
+function calculate() {
+
+			var a = parseInt(document.getElementById("a").value);
+			var b = parseInt(document.getElementById("b").value);
+
+			alert(a+b);
+		}
